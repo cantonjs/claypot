3b290476bdb70166ef4fc4235333a3523ac7e889
3went2 <webb.png@gmail.com>
2017-02-27 14:57:11 +0800

    upgrade babel-register-cli

--- test/custom.js
+++ test/custom.js
@@ -2,8 +2,6 @@
 import koa from 'koa';
 import { logger, config } from '../src';
 
-console.log('furk', config);
-
 export default function server(opts, config) {
 	// logger.info('config', config);
 	logger.info('server started [logger]');
