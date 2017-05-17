032f5f8b5d539bd08cf9f4efe639f84d9171f127
3went2 <webb.png@gmail.com>
2017-05-17 17:58:19 +0800

    switch koa-static-cache to koa-static; upgrade koa-connect-history-api-fallback

--- src/middlewares/static.js
+++ src/middlewares/static.js
@@ -1,9 +1,12 @@
 
-import staticCache from 'koa-static-cache';
+import koaStatic from 'koa-static';
 import config from '../config';
 
 const maxAge = 31536000000; // 365 * 24 * 60 * 60 * 1000
 
-export default (app, options = { maxAge }) =>
-	app.use(staticCache(config.staticDir, options))
+export default (app, options = {}) =>
+	app.use(koaStatic(config.staticDir, {
+		maxAge,
+		...options,
+	}))
 ;
