1ce946e09810ed40e0fd07007219eb3d1516643b
3went2 <webb.png@gmail.com>
2017-05-17 17:31:51 +0800

    update error page

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,4 +1,8 @@
 
 import error from 'koa-error';
+import { resolve } from 'path';
 
-export default (app, options) => app.use(error(options));
+export default (app, options) => app.use(error({
+	template: resolve(__dirname, '../error.html'),
+	...options,
+}));
