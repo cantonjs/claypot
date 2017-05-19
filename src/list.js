fa542e6e44f678522e9f1dc5b7e6a68695e9d57f
3went2 <webb.png@gmail.com>
2017-05-19 15:29:09 +0800

    fix `name` bug in `list` command

--- src/list.js
+++ src/list.js
@@ -11,7 +11,7 @@ export default async function list(options = {}) {
 		],
 		setTable(info) {
 			return [
-				info.name,
+				info.data.name,
 				info.styledStatus,
 				info.crashes,
 				info.memoryUsage.formattedString,
