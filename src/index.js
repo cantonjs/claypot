894736033cad6060db0ecf8612b2cd4c7f5691db
3went2 <webb.png@gmail.com>
2017-06-07 12:15:31 +0800

    add `createApp()` api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 
+import Koa from 'koa';
+
 export start from './start';
 export stop from './stop';
 export list from './list';
 export log from './log';
 export logger, { initLog } from './utils/logger';
 export config, { isDev, isProd } from './config';
+
+export function createApp() {
+	return new Koa();
+}
