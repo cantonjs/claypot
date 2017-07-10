ba10ec3df93a6250cd9b2ec2c98f627d116bd203
3went2 <webb.png@gmail.com>
2017-07-10 17:11:42 +0800

    remove cache .getJson() .setJson() and .wrapJson() methods

--- src/cache.js
+++ src/cache.js
@@ -1,29 +1,8 @@
 
 import cacheManager from 'cache-manager';
-import { isObject, noop } from 'lodash';
+import { noop } from 'lodash';
 import { appLogger } from './utils/logger';
 import createProxyObject from './utils/createProxyObject';
-
-const extendJsonMethods = (cacheStore) => {
-	cacheStore.getJson = async (key) => {
-		const str = await cacheStore.get(key);
-		return str ? JSON.parse(str) : null;
-	};
-	cacheStore.setJson = async (key, value, ...args) => {
-		if (isObject(value)) { value = JSON.stringify(value); }
-		return cacheStore.set(key, value, ...args);
-	};
-	cacheStore.wrapJson = async (key, work, ...args) => {
-		const jsonWork = async () => {
-			let value = await work();
-			if (isObject(value)) { value = JSON.stringify(value); }
-			return value;
-		};
-		const str = await cacheStore.wrap(key, jsonWork, ...args);
-		return str ? JSON.parse(str) : null;
-	};
-	return cacheStore;
-};
 
 const stores = {};
 let cache;
@@ -31,7 +10,7 @@ let cache;
 export function initCache(creators) {
 	creators.forEach(({ key, createCache, options }, index) => {
 		const creater = createCache(options);
-		const cacheStore = extendJsonMethods(cacheManager.caching(creater));
+		const cacheStore = cacheManager.caching(creater);
 		if (!index) { cache = cacheStore; }
 		stores[key] = cacheStore;
 	});
