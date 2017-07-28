18fc23bd1be41de4d5d281b8361f4502abd328d4
3went2 <webb.png@gmail.com>
2017-07-28 19:04:52 +0800

    expose `rewriteConsole` to API

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ export start from './start';
 export stop from './stop';
 export list from './list';
 export log from './log';
-export logger, { initLog, createLogger, inLogsDir } from './utils/logger';
+export logger, {
+	initLog, createLogger, inLogsDir,
+	rewriteConsole, resetConsole, rewriteConsoleInRuntime,
+} from './utils/logger';
 export config, { isDev, isProd } from './config';
 export connectDbs from './dbs/connect';
 export cache, { cacheStores } from './cache';
