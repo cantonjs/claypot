d143c1e9df8131b2076012e9a58d040e5943f06f
3went2 <webb.png@gmail.com>
2018-01-30 11:39:26 +0800

    fix createProxyObject bug

--- src/utils/createProxyObject.js
+++ src/utils/createProxyObject.js
@@ -2,6 +2,9 @@ import logger from 'pot-logger';
 import { isUndefined } from 'lodash';
 
 export default function createProxyObject(source, debugName) {
+	const logError = function logError(name) {
+		logger.error(`${debugName} "${name}" is undefined`);
+	};
 	return new Proxy(
 		{},
 		{
@@ -12,10 +15,19 @@ export default function createProxyObject(source, debugName) {
 						get(_, key) {
 							const dist = source[name];
 							if (isUndefined(dist)) {
-								logger.error(`${debugName} "${name}" is undefined`);
+								logError(name);
 								return {}; // avoid throw `undefined.sth()` like error
 							}
 							return dist[key];
+						},
+						set(_, key, value) {
+							const dist = source[name];
+							if (isUndefined(dist)) {
+								logError(name);
+								return false;
+							}
+							dist[key] = value;
+							return true;
 						},
 					},
 				);
