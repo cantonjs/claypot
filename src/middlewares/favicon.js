04ac2c0c5334d0fc90b2e5eb5a2a63338246d6f3
Webb Peng <webb.png@gmail.com>
2019-05-08 01:09:46 +0800

    refactor: update middleware.favicon

--- src/middlewares/favicon.js
+++ src/middlewares/favicon.js
@@ -1,16 +1,9 @@
-import fav from 'koa-favicon';
 import config from '../config';
 import { resolve } from 'path';
 import { isString } from 'lodash';
 
-const defaults = '../../assets/favicon.ico';
-
 export default (app) => {
 	const { favicon, baseDir } = config;
-	const icon = isString(favicon) ?
-		resolve(baseDir, favicon) :
-		resolve(__dirname, defaults);
-	const faviconFn = fav(icon);
-	faviconFn.keyName = 'favicon';
-	return app.use(faviconFn);
+	const icon = isString(favicon) ? resolve(baseDir, favicon) : undefined;
+	return app.favicon(icon);
 };
